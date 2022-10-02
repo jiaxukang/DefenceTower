1e89a0bb05f26f7d80db875eb828008f9d49aec8
kang <jiaxuk@student.unimelb.edu.au>
2022-10-03 02:14:50 +1100

    aaa

---
+++ src/main/java/org/example/TowerDefense.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javafx.application.Application;
+
+public class TowerDefense {
+
+	public static void main(String[] args) {
+
+		Application.launch(Menu.class, args);
+
+	}
+
+	public static Controller setRelations(View view, String mode) {
+
+		Model model = new Model(mode);
+		Controller controller = new Controller(model);
+
+		return controller;
+	}
+
+}
